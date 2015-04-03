9eddb465efa9589c47effa2bb6dfb61c02c7deda
Marcos da Silva Sampaio <marcos@sampaio.me>
2015-04-03 11:07:31 -0300

    Add Lilypond config
    * Copy files from /Applications/Emacs.app/.../*.el to ~/.emacs.d/site-lisp/lilypond

---
+++ lisp/init-local.el
@@ -0,0 +1,10 @@
+(global-set-key (kbd "C-z") 'undo)
+
+;;; Lilypond mode
+
+(autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
+(add-to-list 'auto-mode-alist '("\\.ly$" . LilyPond-mode))
+(add-to-list 'auto-mode-alist '("\\.ily$" . LilyPond-mode))
+(add-hook 'LilyPond-mode-hook (lambda () (turn-on-font-lock)))
+
+(provide 'init-local)
